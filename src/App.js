d452c7a16d3fde8b343ee2e2d696ca7af2535860
nishimura <saishuheki.nisshing@gmail.com>
2022-03-28 13:53:08 +0900

    [add] worksとpostsを追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import * as styles from "./App.module.css";
+import styles from "./App.module.css";
 import Layout from "./Layout";
-// import styles from "./App.module.css";
-
+import PostItem from "./PostItem";
+import WorkItem from "./WorkItem";
 
 const App = (props) => {
   return (
     <Layout>
-        <p>Hello Gatsby</p>
+        <h2 className={styles.heading}>About</h2>
+
+        <p className={styles.profile}>
+          大阪でバックエンドエンジニアをしています。
+          PHPを使ったwebアプリケーションの作成が得意です。
+        </p>
+
+        <h2 className={styles.heading}>Posts</h2>
+
+        {/* 投稿記事をループで表示する */}
+        {props.posts.map((post) => {
+          return <PostItem post={post} key={post.title} />
+        })}
+
+        <h2 className={styles.heading}>Works</h2>
+
+        <div className={styles.workItems}>
+          {props.works.map((work) => {
+            return <WorkItem work={work} key={work.name} />;
+          })}
+        </div>
     </Layout>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,4 +4,53 @@ import 'minireset.css';
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const posts = [
+    {
+        title: "Next.js Fast Reflesh等のアップデート",
+        date: "2020.10.23",
+        url: "https://qiita.com/aaa",
+    },
+    {
+        title: "Next.js 9.3 新世代の静的サイト生成、Built-in Sassのサポート",
+        date: "2020.03.10",
+        url: "https://qiita.com/bbb",
+    },
+    {
+        title: "Serverless Next.jsをClund Functionにデプロイする",
+        date: "2019.04.28",
+        url: "https://qiita.com/ccc",
+    },
+    {
+        title: "Serverless Next.jsをClund Functionにデプロイする",
+        date: "2019.04.28",
+        url: "https://qiita.com/ccc",
+    }
+];
+
+const works = [
+    {
+        title: "リニューアル！",
+        roles: ['WordPress', 'React'],
+        imageUrl: "./work.png",
+    },
+    {
+        title: "Next.js 9.3 新世代の静的サイト生成、Built-in Sassのサポート",
+        roles: ['React', 'Gatsby', 'Figma'],
+        imageUrl: "./work.png",
+    },
+    {
+        title: "Serverless Next.jsをClund Functionにデプロイする",
+        roles: ['React', 'Next.js'],
+        imageUrl: "./work.png",
+    },
+    {
+        title: "Serverless Next.jsをClund Functionにデプロイする",
+        roles: ['React', 'Next.js'],
+        imageUrl: "./work.png",
+    }
+];
+
+ReactDOM.render(
+    <App posts={posts} works={works} />,
+    document.getElementById("root")
+);
