5df76954e04334845117f7deecaa8ebd1cc63cf4
nishimura <saishuheki.nisshing@gmail.com>
2022-03-29 15:53:09 +0900

    react-helmetのインストール、titleタグ、メタタグの設定

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// ライブラリreact-helmetを読み込み
+import { Helmet } from "react-helmet";
+
 import styles from "./App.module.css";
 import Layout from "./Layout";
 import PostItem from "./PostItem";
@@ -5,28 +8,37 @@ import WorkItem from "./WorkItem";
 
 const App = (props) => {
   return (
-    <Layout>
-        <h2 className={styles.heading}>About</h2>
 
+    // レイアウトの読み込み
+    <Layout>
+
+      {/* ライブラリhelmetの呼び出し */}
+      <Helmet>
+        <title>My Site</title>
+        <meta name="description" content="My first react website"></meta>
+      </Helmet>
+
+        <h2 className={styles.heading}>About</h2>
+        {/* プロフィール */}
         <p className={styles.profile}>
           大阪でバックエンドエンジニアをしています。
           PHPを使ったwebアプリケーションの作成が得意です。
         </p>
 
         <h2 className={styles.heading}>Posts</h2>
-
         {/* 投稿記事をループで表示する */}
         {props.posts.map((post) => {
           return <PostItem post={post} key={post.title} />
         })}
 
         <h2 className={styles.heading}>Works</h2>
-
+        {/* 仕事内容をループで表示する */}
         <div className={styles.workItems}>
           {props.works.map((work) => {
             return <WorkItem work={work} key={work.name} />;
           })}
         </div>
+
     </Layout>
   );
 };
